95b0daf56ad21d5d558b45bbf4ef4633c9ce84a2
Priyambada Roul <roulpriyambada@gmail.com>
2025-02-25 23:42:08 +0530

    adding structure

--- own_chat/llm/routes.py
+++ own_chat/llm/routes.py
@@ -5,6 +5,10 @@ from own_chat.llm.models import MODELS
 llm_blueprint = Blueprint('llm', __name__, url_prefix='/llm')
 
 
+# This route will return a list of all the models in the MODELS list
 @llm_blueprint.route('/llm_models', methods=['GET'])
 def get_llm_models():
     return jsonify([model.serialize() for model in MODELS]), 200
+
+
+
